dccf0963606aa117f829bf14ad6fb0e2edb17335
Carlos Faria <carlosfaria94@users.noreply.github.com>
2018-07-07 11:14:01 +0100

    Ignore reverse flag

--- iperf3tocsv.py
+++ iperf3tocsv.py
@@ -83,29 +83,19 @@ def process(js,csvwriter):
         sent_speed = obj["end"]["sum_sent"]["bits_per_second"] / 1000 / 1000
         rcvd_speed = obj["end"]["sum_received"]["bits_per_second"] / 1000 / 1000
         
-
-        reverse = obj["start"]["test_start"]["reverse"]
-        time = (obj["start"]["timestamp"]["time"]).encode('ascii', 'ignore')
-        cookie = (obj["start"]["cookie"]).encode('ascii', 'ignore')
-        protocol = (obj["start"]["test_start"]["protocol"]).encode('ascii', 'ignore')
+        time = obj["start"]["timestamp"]["time"]
+        cookie = obj["start"]["cookie"]
+        protocol = obj["start"]["test_start"]["protocol"]
         duration = obj["start"]["test_start"]["duration"]
         num_streams = obj["start"]["test_start"]["num_streams"]
-        if reverse not in [0, 1]:
-            sys.exit("unknown reverse")
 
         s = 0
         r = 0
         if ip in db:
             (s, r) = db[ip]
-
-        if reverse == 0:
-            r += rcvd
-            sent = 0
-            sent_speed = 0
-        else:
-            s += sent
-            rcvd = 0
-            rcvd_speed = 0
+        
+        r += rcvd
+        s += sent
 
         db[ip] = (s, r)
 
